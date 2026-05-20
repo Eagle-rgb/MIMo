dcd5e28fb65cbd3f3983e938442e734e01cc7259
Leon Philipp <leonphilipp2001@gmail.com>
2026-05-20 14:52:32 +0200

    Added parameters '--transfer_learning' and '--transferlearning_age' to 'collect_run_statistics' to measure durations of rolling using zero shot cross embodiment testing. Added parameters '--to_npy_run_duration' to convert the statistics .csv file to a .npy file to then use in plotting.

--- results/collect_run_statistics.py
+++ results/collect_run_statistics.py
@@ -21,8 +21,43 @@ if __name__ == '__main__':
     parser.add_argument('--haltung', type=str, choices=['supine', 'prone'], required=True)
     parser.add_argument('--age', type=int, choices=[1,3,6,9], required=True)
     parser.add_argument('--pen_fac', type=float, required=False, default=0.02)
+    parser.add_argument('--to_npy_run_duration', action='store_true', required=False,
+                        help="This option takes an existing **_statistics.csv file in the " \
+                        "current folder ('.'), cuts it to only containing the run durations, " \
+                        "and converts it into a .npy file. Saves the .npy file in the current folder.")
+    parser.add_argument('--duration_until', choices=['lateral', 'full'], required=False,
+                        default='lateral', help="This option selects up to which goal we want to measure " \
+                        "duration. If 'lateral' is selected (the default), we nonetheless only consider " \
+                        "episodes that achieved a full goal, i.e. this option does not affect which " \
+                        "samples we choose.")
+    parser.add_argument('--transfer_learning', action='store_true', required=True, default=False,
+                        help="Test model using different embodiment in a zero-shot setting.")
+    parser.add_argument('--transferlearning_age', choices=[1,3,6,9], type=int, required=False)
     args = parser.parse_args()
 
-    env = make_env(age=args.age, starting_position=args.haltung, pen_fac=args.pen_fac)
-    data = collect_run_statistics_all(env, args.date, args.haltung, args.suffix)
-    data.to_csv(f'{args.date}_{args.haltung}_{args.suffix}_statistics.csv')
+    if args.transfer_learning:
+        output_path_csv = f'{args.date}_{args.haltung}_{args.suffix}_transferlearning_age{args.transferlearning_age}_statistics.csv'
+    else:
+        output_path_csv = f'{args.date}_{args.haltung}_{args.suffix}_statistics.csv'
+
+    if not args.to_npy_run_duration:
+        age = args.age if not args.transfer_learning else args.transferlearning_age
+        env = make_env(age=age, starting_position=args.haltung, pen_fac=args.pen_fac)
+        data = collect_run_statistics_all(env, args.date, args.haltung, args.suffix)
+        data.to_csv(output_path_csv)
+
+    else:
+        # Load existing .csv data.
+        data = pd.read_csv(output_path_csv, index_col=['Run', 'Episode'])
+        # Reduce to only successful episodes.
+        successful_df = data[data['Success'] == True]
+        # Get durations until goal, i.e. lateral or full roll.
+        if args.duration_until == 'lateral':
+            durations = successful_df['Time_SideLying']
+        else:
+            durations = successful_df['Time']
+
+        if args.transfer_learning:
+            np.save(f'duration_{args.duration_until}_{args.haltung}_{args.suffix}_transferlearning_age{args.transferlearning_age}.npy')
+        else:
+            np.save(f'duration_{args.duration_until}_{args.haltung}_{args.suffix}.npy')
